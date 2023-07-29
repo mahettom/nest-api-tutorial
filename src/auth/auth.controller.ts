34c42a0aae11e6ebd1291df2cff4689611bf6aa8
TomMahe <mahettom@gmail.com>
2023-07-29 13:26:59 +0200

    .

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, ParseIntPipe, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 // —————————————————————————————————————————————————————— Controller -> /auth/...
 @Controller('auth')
@@ -8,7 +9,7 @@ export class AuthController {
 
   // ———————————————————————————————————————————————————— Post -> /auth/signup
   @Post('signup')
-  signup() {
+  signup(@Body() dto: AuthDto) {
     return this.authService.signup();
   }
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
