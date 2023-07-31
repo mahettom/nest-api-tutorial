33ab5a5cd9c79e029ed477c171a0854282b7f095
TomMahe <mahettom@gmail.com>
2023-07-31 13:37:20 +0200

    return access token to user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,19 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
-  // —————————————————————————————————————————————— CALLED WHEN USER MAKE A REQUEST BY THE CONTROLLER (auth.controller.ts l-10)
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
+  // —————————————————————————————————— SIGNUP ————— CALLED WHEN USER MAKE A REQUEST BY THE CONTROLLER (auth.controller.ts l-10)
   async signup(dto: AuthDto) {
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ generate password
     const hash = await argon.hash(dto.password);
@@ -19,9 +25,8 @@ export class AuthService {
           hash,
         },
       });
-      // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ return user saved without hash
-      delete user.hash;
-      return user;
+      // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ give token to the user
+      return this.signToken(user.id, user.email);
       // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ prisma doc for list of error code
       // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ this one -> try to assign an email twice but email was plan to be unique
     } catch (error) {
@@ -35,7 +40,7 @@ export class AuthService {
     }
   }
 
-  // —————————————————————————————————————————————— CALLED WHEN USER MAKE A REQUEST BY THE CONTROLLER (auth.controller.ts l-18)
+  // —————————————————————————————————— SIGNIN ——————— CALLED WHEN USER MAKE A REQUEST BY THE CONTROLLER (auth.controller.ts l-18)
   async signin(dto: AuthDto) {
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ find user by id
     const user = await this.prisma.user.findUnique({
@@ -46,15 +51,33 @@ export class AuthService {
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ not found -> alert user
     if (!user)
       throw new ForbiddenException(
-        'No account iss register with this email, try to signin',
+        'No account is register with this email, try to signup',
       );
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ found -> compare password
     const pwMatches = await argon.verify(user.hash, dto.password);
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ password incorrect -> throw exception
     if (!pwMatches)
       throw new ForbiddenException('password seems to be incorect');
-    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ password correct -> send back user
-    delete user.hash;
-    return user;
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ password correct -> send the sign token to the user
+    // delete user.hash;
+    return this.signToken(user.id, user.email);
+  }
+
+  async signToken(userId: number, email): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    const secret = this.config.get('JWT_SECRET');
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: secret,
+    });
+
+    return {
+      access_token: token,
+    };
   }
 }
