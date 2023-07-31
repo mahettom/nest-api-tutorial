468ca5a870c92bf162c4e038f386b27b545be3a6
TomMahe <mahettom@gmail.com>
2023-07-31 13:35:53 +0200

    .

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
 import { Module } from '@nestjs/common';
 
 @Module({
+  imports: [JwtModule.register({})],
   controllers: [AuthController],
   providers: [AuthService],
 })
