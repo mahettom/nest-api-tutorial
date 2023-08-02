2fe6b25a08e302db5e1a2c889762af11bf32a79c
TomMahe <mahettom@gmail.com>
2023-08-02 12:47:07 +0200

    Created custom decorator -> getUser

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,17 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { JwtGuard } from '../auth/guard';
-import { Request } from 'express';
+import { GetUser } from 'src/auth/decorator';
+import { User } from '@prisma/client';
 
+// ———————————————————————————————————————————————————— Only access under if token valid
+@UseGuards(JwtGuard)
 // —————————————————————————————————————————————————————— Controller -> prefix by /user/...
 @Controller('user')
 export class UserController {
-  // ———————————————————————————————————————————————————— Only access the route under if token valid
-  @UseGuards(JwtGuard)
   // ———————————————————————————————————————————————————— Get -> /user/me
   @Get('me')
   // —————————————————————————————————— request from jwt.strategy the user return from the validate function
-  getMe(@Req() req: Request) {
-    return req.user;
+  getMe(@GetUser() user: User) {
+    return user;
   }
 }
