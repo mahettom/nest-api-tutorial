b5bc8eb2d1b0e3facd2b9b00c15ebf0324bfde80
TomMahe <mahettom@gmail.com>
2023-08-03 18:05:38 +0200

    implement e2e test ->  signup & signin

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,13 +1,17 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { PrismaService } from '../src/prisma/prisma.service';
 import { AppModule } from '../src/app.module';
+import { AuthDto } from 'src/auth/dto';
 import { Test } from '@nestjs/testing';
+import * as pactum from 'pactum';
 
 describe('App e2e', () => {
-  // ———————————————————————————————————————— Precise app gonna have the type of a nest app
   let app: INestApplication;
-  // ———————————————————————————————————————— Create a test module & import AppModule
-  // ———————————————————————————————————————— This will give access to all files available by AppModule
+  let prisma: PrismaService;
+
   beforeAll(async () => {
+    // ———————————————————————————————————————— Create a test module & import AppModule
+    // ———————————————————————————————————————— This will give access to all files available by AppModule
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -20,10 +24,108 @@ describe('App e2e', () => {
       }),
     );
     await app.init();
+    await app.listen(3333);
+
+    prisma = app.get(PrismaService);
+
+    await prisma.cleanDb();
+    pactum.request.setBaseUrl('http://localhost:3333');
   });
 
   afterAll(() => {
     app.close();
   });
-  it.todo('should pass');
+
+  // ////////////////////////////////     AUTH      //////////////////////////////
+  describe('Auth', () => {
+    const dto: AuthDto = {
+      password: '123',
+      email: 'testguy@getMaxListeners.com',
+    };
+    // ////////////////////////////////////////////////////////// SIGNUP /////////
+    describe('Signup', () => {
+      // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  400  ~~~~~~~~~~~~~~~~~~~~~~~~
+      it('should throw if email empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            password: dto.password,
+          })
+          .expectStatus(400);
+      }); // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  400  ~~~~~~~~~~~~~~~~~~~~~~~~
+      it('should throw if pasword empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400);
+      }); // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  400  ~~~~~~~~~~~~~~~~~~~~~~~~
+      it('should throw if any info provided', () => {
+        return pactum.spec().post('/auth/signup').expectStatus(400);
+      });
+      // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  201  ~~~~~~~~~~~~~~~~~~~~~~~~
+      it('should signup', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+      });
+    });
+    // ////////////////////////////////////////////////////////// SIGNIN /////////
+    describe('Signin', () => {
+      // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  400  ~~~~~~~~~~~~~~~~~~~~~~~~
+      it('should throw if email empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody({
+            password: dto.password,
+          })
+          .expectStatus(400);
+      }); // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  400  ~~~~~~~~~~~~~~~~~~~~~~~~
+      it('should throw if pasword empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400);
+      }); // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  400  ~~~~~~~~~~~~~~~~~~~~~~~~
+      it('should throw if any info provided', () => {
+        return pactum.spec().post('/auth/signin').expectStatus(400);
+      });
+      it('should signin', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(dto)
+          .expectStatus(200);
+      });
+    });
+  });
+
+  // ////////////////////////////////     USER      //////////////////////////////
+  describe('User', () => {
+    describe('Get me', () => {});
+
+    describe('Edit user', () => {});
+  });
+
+  // //////////////////////////////     BOOKMARK      ////////////////////////////
+  describe('Bookmarks', () => {
+    describe('Create bookmarks', () => {});
+
+    describe('Get bookmarks', () => {});
+
+    describe('Get bookmarks by id', () => {});
+
+    describe('Edit bookmark', () => {});
+
+    describe('Delete bookmark', () => {});
+  });
 });
