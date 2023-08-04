720eeb049542b31c8b81fb3524853ff46851efcd
TomMahe <mahettom@gmail.com>
2023-08-04 13:11:29 +0200

    add in user dto | email | firstname | lastname

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { JwtGuard } from '../auth/guard';
 import { GetUser } from '../auth/decorator';
 import { User } from '@prisma/client';
@@ -15,6 +15,6 @@ export class UserController {
     return user;
   }
 
-  // @Patch()
-  // editUser() {}
+  @Patch()
+  editUser() {}
 }
