21403cf5c2d3dec62e54892664ef0894694715e3
TomMahe <mahettom@gmail.com>
2023-08-04 12:56:10 +0200

    add test -> Get users/me

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -104,14 +104,25 @@ describe('App e2e', () => {
           .spec()
           .post('/auth/signin')
           .withBody(dto)
-          .expectStatus(200);
+          .expectStatus(200)
+          .stores('userAt', 'access_token');
       });
     });
   });
 
   // ////////////////////////////////     USER      //////////////////////////////
   describe('User', () => {
-    describe('Get me', () => {});
+    describe('Get me', () => {
+      it('should get current user', () => {
+        return pactum
+          .spec()
+          .get('/users/me')
+          .withHeaders({
+            Authorization: 'Bearer $S{userAt}',
+          })
+          .expectStatus(200);
+      });
+    });
 
     describe('Edit user', () => {});
   });
@@ -124,8 +135,8 @@ describe('App e2e', () => {
 
     describe('Get bookmarks by id', () => {});
 
-    describe('Edit bookmark', () => {});
+    describe('Edit bookmark by id', () => {});
 
-    describe('Delete bookmark', () => {});
+    describe('Delete bookmark by id', () => {});
   });
 });
